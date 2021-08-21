d66898121a24e2ad05090ac884c94d24cc004e6b
Hironao OTSUBO <motemen@gmail.com>
2021-08-21 09:02:59 +0900

    tools

---
+++ tools.go
@@ -0,0 +1,6 @@
+//+build tools
+
+package tools
+
+// replace github.com/budougumi0617/nrseg => github.com/motemen/nrseg v0.0.6-0.20210820234132-47794bd27717
+import "github.com/budougumi0617/nrseg/cmd/nrseg"
